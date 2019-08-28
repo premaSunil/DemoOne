0f44541ab2e7b89e00d0c96a1617c445f0148a18
Bharat <bharatshankar5@gmail.com>
2019-08-28 12:25:57 +0530

    again

--- DemoOne/ViewController.swift
+++ DemoOne/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.blue
         //this is second try
         //sel
         // Do any additional setup after loading the view.
